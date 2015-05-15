227ce5e8f1dd9e357e9bce36c8d4c0f2eac9291c
quasto <sergio@arduino.org>
2015-05-15 11:32:09 +0200

    update localization entries

--- modules/Localization/nls/it/strings.js
+++ modules/Localization/nls/it/strings.js
@@ -5,6 +5,8 @@
 
 
 define({
+
+    /* MENU and MENU ITEMS */
     "ARDUINO_MENU_SKETCH"               : "Sketch",
     "ARDUINO_MENU_SKETCH_BUILD"         : "Verifica / Compila",
     "ARDUINO_MENU_SKETCH_ADDFILE"       : "Aggiungi file",
@@ -16,6 +18,33 @@ define({
     "ARDUINO_MENU_EDIT_COPYHTML"        : "Copia come HTML",
     "ARDUINO_MENU_EDIT_FINDSELECTED"    : "Trova testo selezionato",
 
+    "ARDUINO_MENU_TOOLS"                    : "Strumenti",
+    "ARDUINO_MENU_TOOLS_AUTOFORMATTING"     : "Auto formattazione",
+    "ARDUINO_MENU_TOOLS_STORESKETCH"        : "Archivia sketch",
+    "ARDUINO_MENU_TOOLS_SERIALMONITOR"      : "Monitor serial",
+    "ARDUINO_MENU_TOOLS_SELECTBOARD"        : "Scheda",
+    "ARDUINO_MENU_TOOLS_SELECTPORT"         : "Porta",
+    "ARDUINO_MENU_TOOLS_SELECTPROGRAMMER"   : "Programmatore",
+    "ARDUINO_MENU_TOOLS_BURNBOOTLOADER"     : "Scrivi bootloader",
+
+    "ARDUINO_MENU_FILE"                     : "File",
+    "ARDUINO_MENU_FILE_OPENSAMPLES"         : "Apri esempi",
+    "ARDUINO_MENU_FILE_UPLOAD"              : "Carica",
+    "ARDUINO_MENU_FILE_UPLOADUSEPROGR"      : "Carica tramite programmatore",
+    "ARDUINO_MENU_FILE_PRINTPAGESETTING"    : "Impostazioni pagina per la stampa",
+    "ARDUINO_MENU_FILE_PRINT"               : "Stampa",
+    "ARDUINO_MENU_FILE_PREFERENCES"         : "Preferenze",
+
+    "ARDUINO_MENU_HELP_ABOUT"               : "Arduino",
+
+    /* DIALOGS */
+    "ARDUINO_DIALOG_SELECT_FILE"            : "Seleziona file",
+    "ARDUINO_DIALOG_SELECT_FOLDER"          : "Seleziona cartella",
+    "ARDUINO_DIALOG_SELECT_ARCHIVE"         : "Seleziona archivio zip",
+    "ARDUINO_DIALOG_IMPORT_LIBS"            : "Importa librerie",
+    "ARDUINO_DIALOG_ARDUINO"                : "Arduino",
+
+    /* EXTRAS */
     "ARDUINO_EXTRA_COMING_SOON"         : "Disponibile a breve",
     "ARDUINO_EXTRA_WIP"                 : "In lavorazione"
 });
--- modules/Localization/nls/root/strings.js
+++ modules/Localization/nls/root/strings.js
@@ -4,6 +4,8 @@
 /*global define */
 
 define({
+
+    /* MENU and MENU ITEMS */
     "ARDUINO_MENU_SKETCH"               : "Sketch",
     "ARDUINO_MENU_SKETCH_BUILD"         : "Verify / Compile",
     "ARDUINO_MENU_SKETCH_ADDFILE"       : "Add file",
@@ -15,6 +17,33 @@ define({
     "ARDUINO_MENU_EDIT_COPYHTML"        : "Copy as HTML",
     "ARDUINO_MENU_EDIT_FINDSELECTED"    : "Find selected text",
 
+    "ARDUINO_MENU_TOOLS"                    : "Tools",
+    "ARDUINO_MENU_TOOLS_STORESKETCH"        : "Store sketch",
+    "ARDUINO_MENU_TOOLS_AUTOFORMATTING"     : "Autoformatting",
+    "ARDUINO_MENU_TOOLS_SERIALMONITOR"      : "Serial monitor",
+    "ARDUINO_MENU_TOOLS_SELECTBOARD"        : "Board",
+    "ARDUINO_MENU_TOOLS_SELECTPORT"         : "Port",
+    "ARDUINO_MENU_TOOLS_SELECTPROGRAMMER"   : "Programmer",
+    "ARDUINO_MENU_TOOLS_BURNBOOTLOADER"     : "Burn bootloader",
+
+    "ARDUINO_MENU_FILE"                     : "File",
+    "ARDUINO_MENU_FILE_OPENSAMPLES"         : "Open samples",
+    "ARDUINO_MENU_FILE_UPLOAD"              : "Upload",
+    "ARDUINO_MENU_FILE_UPLOADUSEPROGR"      : "Upload using programmer",
+    "ARDUINO_MENU_FILE_PRINTPAGESETTING"    : "Print page settings",
+    "ARDUINO_MENU_FILE_PRINT"               : "Print",
+    "ARDUINO_MENU_FILE_PREFERENCES"         : "Preferences",
+
+    "ARDUINO_MENU_HELP_ABOUT"               : "About Arduino",
+
+    /* DIALOGS */
+    "ARDUINO_DIALOG_SELECT_FILE"            : "Select file",
+    "ARDUINO_DIALOG_SELECT_FOLDER"          : "Select folder",
+    "ARDUINO_DIALOG_SELECT_ARCHIVE"         : "Seleziona zip archive",
+    "ARDUINO_DIALOG_IMPORT_LIBS"            : "Import libraries",
+    "ARDUINO_DIALOG_ARDUINO"                : "Arduino",
+
+    /* EXTRAS */
     "ARDUINO_EXTRA_COMING_SOON"         : "Coming soon",
     "ARDUINO_EXTRA_WIP"                 : "Work in progress"
 });
