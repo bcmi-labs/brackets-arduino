a95831e81f832fd88d2cc11ccb19e141685aaa8d
quasto <sergio@arduino.org>
2015-05-15 13:41:04 +0200

    forgot file in the previous commit

--- modules/Extra/revision.js
+++ modules/Extra/revision.js
@@ -1,12 +1,7 @@
 define(function (require, exports, module) {
 "use strict";
-module.exports =
-{
-  "version"	    :	2000000, //version number
-  "hversion"	: 	"2.0.0", //human-readable version number
-  "cycle"		: 	"alpha", //cycle
-  "source"      :   ""      //source code repo
-}
+
+  module.exports = JSON.parse( require("text!../../shared/revision") );
 
 });
 
